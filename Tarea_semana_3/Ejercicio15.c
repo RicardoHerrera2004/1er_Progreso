ac296016a8e84b3ca6b40fd6b434958afb95ecf8
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-10-16 00:25:52 -0500

    Segunda version del codigo del ejercicio 15

---
+++ Tarea_semana_3/Ejercicio15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void) {
+
+    int resultado=0;
+    int contador;
+
+    for (contador = 1; contador <= 20; contador++)
+    {
+        resultado=resultado+3*contador;
+    }
+    
+    printf ("El resultado de la suma de los primeros 20 multiplos de 3 es: %d", resultado);
+
+    return 0;
+}
