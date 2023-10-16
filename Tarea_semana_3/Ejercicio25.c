b1b4c83efabbe19de3373efd481ea46ac799c9ee
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-10-16 00:55:31 -0500

    Cuarta version del codigo del ejercicio 25

--- Tarea_semana_3/Ejercicio25.c
+++ Tarea_semana_3/Ejercicio25.c
@@ -4,18 +4,22 @@ int main (void) {
 
     int numero;
     int digito;
+    int contador;
     int resultado;
+    int digito_total=0;
     printf ("Ingrese un numero: ");
     scanf ("%d", &numero);
     
     while (numero > 0)
     {
+        contador++;
         digito = numero%10;
-        printf ("El numero es: %d \n", digito);
+        printf ("El digito es: %d \n", digito);
+        digito_total=digito_total+digito;
         numero =numero/10;
     }
     
-
+printf ("La suma de los digito es igual a: %d \n", digito_total);
 
     return 0;
 }
