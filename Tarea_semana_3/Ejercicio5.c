89948731079d4aee02729447f71f4a24e286115c
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-10-16 00:15:50 -0500

    Quinta version del codigo del ejercicio 5

--- Tarea_semana_3/Ejercicio5.c
+++ Tarea_semana_3/Ejercicio5.c
@@ -7,19 +7,26 @@ int main (void) {
     int resultados;
 
     printf ("Bienvenido al programa en donde se mostrara los numeros terminados en 4 de dos numeros comprendidos \n");
-    printf ("Ingrese el primer numero (El numero el cual iniciara el intervalo): ");
-    scanf ("%d", &numero1);
-    printf ("Ingrese el segundo numero (El numero que finalizara el intervalo): ");
-    scanf ("%d", &numero2);
-    printf ("Los numeros que terminan en 4 y se encuentran en este intervalo son: \n");
+    do
+    {
+        printf ("Ingrese el primer numero (El numero el cual iniciara el intervalo): ");
+        scanf ("%d", &numero1);
+        printf ("Ingrese el segundo numero (El numero que finalizara el intervalo): ");
+        scanf ("%d", &numero2);
+        if (numero1==numero2) {
+            printf ("Ingrese un numero diferente para realizar la comprension de los numeros. \n");
+        }
+    } while (numero1 == numero2);
 
-    for (numero1 = 0; numero1 <= numero2; numero1++)
+    printf ("Los numeros que terminan en 4 y se encuentran en este intervalo son: \n");
+      
+    for (numero1 = numero1; numero1 <= numero2; numero1++)
     {
         resultados=numero1;
-        if (resultados%10==4)
+        if (resultados%10==4 || resultados%10==-4)
         {
             printf ("%d\n", resultados);
-        }
+        }         
         resultados++;
     }
     
