0cc5dfd6db3df3585d59a3b34db8c570edcc124c
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-10-16 21:09:44 -0500

    Segunda version del codigo del ejercicio 35

--- Tarea_semana_3/Ejercicio35.c
+++ Tarea_semana_3/Ejercicio35.c
@@ -2,7 +2,16 @@
 
 int main (void) {
 
+    int numero1;
+    int numero2;
+    
+    printf ("Calculador del producto mutuo del digito de dos numeros enteros. \n");
+    printf ("Ingrese el primer numero: ");
+    scanf ("%d", &numero1);
+    printf ("Ingrese el segundo numero: ");
+    scanf ("%d", &numero2);
 
+    printf ("El primer numero es: %d, y el segundo numero es: %d \n", numero1, numero2);
 
     return 0;
 }
